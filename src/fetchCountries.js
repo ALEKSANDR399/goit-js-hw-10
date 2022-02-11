b3c1f5ba069b89f766be37da84a387d23144d60f
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-02-11 18:33:01 +0200

    ДЗ№10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,10 +6,13 @@ export default class NewsApiService {
   fetchCountries() {
     const url = `https://restcountries.com/v3.1/name/${this.country}?fields=name,capital,population,flags,languages`;
     return fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        return data;
-      });
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(error => console.error(error));
   }
 
   get nameCountry() {
--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import './css/styles.css';
 import NewsApiService from './fetchCountries';
 import markupSearch from './templates/markup-search.hbs';
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
+
 const DEBOUNCE_DELAY = 300;
 const inputSearchBox = document.querySelector('#search-box');
 const countriesList = document.querySelector('.country-list');
 const newApiService = new NewsApiService();
 
-inputSearchBox.addEventListener('input', addInputToFetch);
+inputSearchBox.addEventListener('input', debounce(addInputToFetch, DEBOUNCE_DELAY));
 
 function addInputToFetch(e) {
-  newApiService.nameCountry = e.currentTarget.value;
+  newApiService.nameCountry = e.target.value;
 
-  newApiService.fetchCountries().then(data => paramsCountry(data));
+  newApiService.fetchCountries().then(data => {
+    if (e.target.value.length === 1) {
+      return alert('Too many matches found. Please enter a more specific name.');
+    }
+    paramsCountry(data);
+  });
 }
 
 function paramsCountry(data) {
-  return data
+  const markup = data
     .map(({ flags, name }) => {
       const { svg } = flags;
       const { official } = name;
-      console.log(svg);
-      console.log(official);
-      return `<img src="${svg}" alt="flag">
-      <h2>${official}</h2>`;
+
+      return `<img src="${svg}" alt="flag" width="60">
+    <h2>${official}</h2>`;
     })
     .join('');
-}
 
-countriesList.innerHTML('afterend', paramsCountry(data));
+  countriesList.insertAdjacentHTML('beforeend', markup);
+}
